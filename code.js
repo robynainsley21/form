95f0b4923102e89da20ab85f33294f365eb215e0
robynainsley21 <robynainsley2i@gmail.com>
2024-05-14 11:57:17 +0200

    Create form

---
+++ code.js
@@ -0,0 +1,34 @@
+function submitForm() {
+  let firstInput = document.getElementById("fname").value;
+  let lastInput = document.getElementById("lname").value;
+//   let firstName = document.getElementById("firstName");
+//   let lastName = document.getElementById("lastName");
+  let parent = document.getElementById("parentContainer");
+
+  const allPeople = [];
+
+  const person = {
+    first: firstInput,
+    last: lastInput,
+  };
+
+  allPeople.push(person);
+
+  for (let bio of allPeople) {
+    let nameParagraph = document.createElement("p");
+    let surnameParagraph = document.createElement("p");
+
+    // firstName
+    nameParagraph.textContent = `First name: ${bio.first}`;
+    parent.appendChild(nameParagraph);
+
+    // lastName
+    surnameParagraph.textContent = `Last name: ${bio.last}`;
+    parent.appendChild(surnameParagraph);
+
+    console.log(allPeople)
+    // console.log(`Last name: ${bio.last}`)
+  }
+
+}
+// submit()
